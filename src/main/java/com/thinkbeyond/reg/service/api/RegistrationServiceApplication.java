b16b9cc29132a03a4818ebfaa82ebe29c2fd637f
Reddy <beerreddy@deloitte.com>
2022-10-04 17:24:25 +0530

    added swagger documentation

--- src/main/java/com/thinkbeyond/reg/service/api/RegistrationServiceApplication.java
+++ src/main/java/com/thinkbeyond/reg/service/api/RegistrationServiceApplication.java
@@ -1,10 +1,12 @@
 package com.thinkbeyond.reg.service.api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class RegistrationServiceApplication {
 
     public static void main(String[] args) {
